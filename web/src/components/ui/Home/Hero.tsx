c4c94bed401bcfe2a35dfe090aeadf83c6052719
Tyno <116711205+Tyno1@users.noreply.github.com>
2024-11-29 22:36:09 +0000

    created food catalogue

--- web/src/components/ui/Home/Hero.tsx
+++ web/src/components/ui/Home/Hero.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import img1 from "@/assets/hero/lum3n-cHc5H3_FKhs-unsplash.jpg";
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
@@ -15,23 +17,54 @@ export default function Hero() {
         />
         <div className="absolute inset-0 bg-black bg-opacity-50" />
         <div className="relative flex flex-col items-start z-10 w-full md:w-[60%] md:ml-10 p-4">
-          <h1 className="font-black text-6xl md:text-7xl lg:text-8xl mb-4 leading-tight">
+          <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ duration: 1, type: "spring", bounce: 0.6 }}
+            className="font-black text-6xl md:text-7xl lg:text-8xl mb-4 leading-tight"
+          >
             Taste Your City's
             <br />
             Hidden Flavors
-          </h1>
-          <p className="text-lg md:text-lg mb-6 max-w-2xl ">
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 25 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ delay: 0.1, duration: 1, ease: "easeInOut" }}
+            className="text-lg md:text-lg mb-6 max-w-2xl "
+          >
             Embark on a culinary journey through your neighborhood. Discover
             mouthwatering dishes shared by food enthusiasts and local
             restaurants, brought to life by immersive AI-powered audio reviews.
-          </p>
+          </motion.p>
           <div className="flex flex-col sm:flex-row gap-4">
-            <button aria-label="explore-local-flavors" className="bg-transparent border-2 border-white hover:border-red hover:text-white text-white px-6 py-3 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:bg-red focus:border-none ">
+            <motion.button
+              initial={{ opacity: 0, y: 25 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{ delay: 0.1, duration: 1, ease: "easeInOut" }}
+              aria-label="explore-local-flavors"
+              className="bg-transparent border-2 border-white hover:border-red hover:text-white text-white px-6 py-3 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:bg-red focus:border-none"
+            >
               Explore Local Flavors
-            </button>
-            <button aria-label="listen-to-tasty-reviews" className="bg-transparent border-2 border-white hover:border-red hover:text-white text-white px-6 py-3 rounded-md transition duration-300 ease-in-out transform hover:scale-105 focus:bg-red focus:border-none ">
+            </motion.button>
+            <motion.button
+              initial={{ opacity: 0, y: 25 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.1, duration: 1, ease: "easeInOut" }}
+              aria-label="listen-to-tasty-reviews"
+              className="bg-transparent border-2 border-white hover:border-red hover:text-white text-white px-6 py-3 rounded-md transition duration-300 ease-in-out"
+            >
               Listen to Tasty Reviews
-            </button>
+            </motion.button>
           </div>
         </div>
       </div>
